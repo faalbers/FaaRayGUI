d89f6908bd686c2a42df7dd0e2eadb68f5e5055c
faalbers <chalbers@gmail.com>
2021-07-31 12:48:01 -0700

    FaaRay update to 0.1.2

--- RenderThread.cpp
+++ RenderThread.cpp
@@ -17,12 +17,7 @@ RenderThread::~RenderThread()
 
 void RenderThread::run()
 {
-    auto t1 = std::chrono::high_resolution_clock::now();
     renderJobSPtr_->render();
-    auto t2 = std::chrono::high_resolution_clock::now();
-
-    auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
-    std::cout << "Render Time: " << ms_int.count() << "ms\n";
 
     emit renderDone();
 }
